b108248f07f33ac0e6f89a1694f6dd2ca8089854
Alexandre René <alexandre.rene@freeneurons.ca>
2018-08-01 18:43:15 +0200

    [GD] Cleanup. Allow computing output filenames for old files with special case.

--- fsGIF/gradient_descent.py
+++ fsGIF/gradient_descent.py
@@ -95,7 +95,13 @@ def get_model(params):
 
     params = ml.parameters.params_to_arrays(params)
 
-    post_params = params.posterior
+    post_params = params.posterior.copy()
+
+    # HACK because we changed spike.params format; reverts to old format if
+    # input is a parameterized history function
+    # if 'type' in post_params.data.params.input:
+    #     post_params.data.params.input = post_params.data.params.input.params
+    # END HACK
 
     if 'init_vals' not in params or params.init_vals is None:
         init_vals = None
@@ -283,7 +289,7 @@ def get_sgd(params, model, pymc_model, start_var, batch_size_var):
         def model_initialize(t):
             model.initialize(t=t)
 
-    if params.sgd.cost == 'loglikelihood':
+    if params.sgd.cost in ('loglikelihood', 'logL', 'log likelihood', 'log L'):
         cost = shim.cast_floatX(pymc_model.n.logpt)
     elif params.sgd.cost == 'posterior':
         cost = shim.cast_floatX(pymc_model.logpt)
@@ -339,209 +345,42 @@ def get_sgd(params, model, pymc_model, start_var, batch_size_var):
 
     return sgd
 
-# def get_sgd(mgr):
-#     get_sgd_legacy(mgr.params)
-#
-# def get_sgd_legacy(params, prev_run=None):
-#
-#     model, prior_sampler = get_model(params)
-#
-#     post_params = params.posterior
-#     prior_descs = params.posterior.model.prior
-#
-#     fitmask = get_fitmask(model, post_params.mask)
-#
-#     if prev_run is None:
-#         # For a new run, a new sgd instance must be created and initialized
-#
-#         sgd = gd.SGD_old(
-#             cost = model.loglikelihood,
-#             cost_format = 'logL',
-#             optimizer = params.optimizer,
-#             model = model,
-#             start = params.posterior.burnin,
-#             datalen = params.posterior.datalen,
-#             burnin = params.batch_burnin,
-#             mbatch_size = params.batch_size
-#         )
-#         sgd.set_fitparams(fitmask)
-#
-#         transform_descs = [desc.transform for desc in prior_descs.values() if hasattr(desc, 'transform')]
-#         # TODO: Change SGD to use the TransformedVar class, than remove the following
-#         transforms = []
-#         for desc in transform_descs:
-#             origname, newname = desc.name.split('->')
-#             transforms.append([origname.strip(), newname.strip(), desc.to, desc.back])
-#         for transform_desc in transforms:
-#             try:
-#                 var = sgd.get_param(transform_desc[0])
-#             except KeyError:
-#                 pass
-#             else:
-#                 sgd.transform(var, *transform_desc[1:])
-#         # for time_cst in [getattr(model.params, tc_name) for tc_name in post_params.time_constants]:
-#         #     if time_cst in fitmask:
-#         #         sgd.transform( time_cst, 'log' + time_cst.name,
-#         #                        'τ -> shim.log10(τ)', 'logτ -> 10**logτ' )
-#
-#         sgd.verify_transforms(trust_automatically=True)
-#             # FIXME: Use TransformedVar, and then this whole verification thing won't be needed
-#
-#         # If the parameters which generated the data are known, set them as ground truth
-#         if ( 'params' in post_params.data and isinstance(post_params.data, ParameterSet)
-#              and 'model' in post_params.data.params
-#              and isinstance(post_params.data.params, ParameterSet) ):
-#             #trueparams = core.get_model_params(post_params.data.params.model,
-#             #                                   post_params.model.type)
-#             # >>>> HACK <<<<<< Above doesn't work e.g. for hetero parameters, which
-#             #                  aren't compatible with the meso model
-#             trueparams = core.get_model_params(post_params.model.params,
-#                                                post_params.model.type)
-#             sgd.set_ground_truth(trueparams)
-#
-#         if init_vals is not None:
-#             init_vals_format = getattr(init_vals, 'format', 'cartesian')
-#             init_vals_random = getattr(init_vals, 'random', False)
-#             if init_vals_random:
-#                 if ( 'seed' in init_vals and init_vals.seed is not None ):
-#                     np.random.seed(init_vals.seed)
-#                 logger.debug("RNG state: {}".format(np.random.get_state()[1][0]))
-#
-#             if init_vals_format == 'prior':
-#                 _init_vals_dict = {}
-#                 # TODO: Checking for substitutions would be cleaner if using TransformedVar
-#                 for name in post_params.variables:
-#                     var = sgd.get_param(name)
-#                     if var in sgd.substitutions:
-#                         _init_var = sgd.substitutions[var][0]
-#                         _init_vals_dict[_init_var] = prior_sampler(var)
-#                     else:
-#                         _init_vals_dict[var] = prior_sampler(var)
-#
-#             elif init_vals_format == 'cartesian':
-#                 _init_vals_dict = { sgd.get_param(pname): init_vals[pname]
-#                                     for pname in init_vals.variables }
-#
-#             elif init_vals_format in ['polar', 'spherical']:
-#
-#                 # The total number of variables is the sum of each variable's number of elements
-#                 curvals = OrderedDict( (varname, sgd.get_param(varname).get_value())
-#                                        for varname in init_vals.variables )
-#                 nvars = sum( np.prod(var.shape) if hasattr(var, 'shape') else 1
-#                              for var in curvals.values() )
-#
-#                 # Get the coordinate angles
-#                 if init_vals_random:
-#                     # All angles except last [0, π)
-#                     # Last angle [0, 2π)
-#                     angles = np.uniform(0, np.pi, nvars - 1)
-#                     angles[-1] = 2*angles[-1]
-#                 else:
-#                     # The angles may be given with nested structure; this is just to help
-#                     # legibility, so flatten everything.
-#                     angles = np.concatenate([np.array(a).flatten() for a in init_vals.angles])
-#                     if len(angles) != nvars - 1:
-#                         raise ValueError("Number of coordinate angles (currently {}) must be "
-#                                          "one less than the number of variables. (currently {})."
-#                                          .format(len(init_vals.angles), len(init_vals.variables)))
-#
-#                 # Compute point on the unit sphere
-#                 sines = np.concatenate(([1], np.sin(angles)))
-#                 cosines = np.concatenate((np.cos(angles), [1]))
-#                 unit_vals_flat = np.cumprod(sines) * cosines
-#                 # "unflatten" the coordinates
-#                 unit_vals = []
-#                 i = 0
-#                 for name, val in curvals.items():
-#                     if shim.isscalar(val):
-#                         unit_vals.append(unit_vals_flat[i])
-#                         i += 1
-#                     else:
-#                         varlen = np.prod(val.shape)
-#                         unit_vals.append(unit_vals_flat[i:i+varlen].reshape(val.shape))
-#                         i += varlen
-#
-#                 # rescale coords
-#                 radii = []
-#                 for varname, var in curvals.items():
-#                     radius = init_vals.radii[varname]
-#                     if shim.isscalar(var):
-#                         radii.append(radius)
-#                     else:
-#                         if shim.isscalar(radius):
-#                             radii.append( np.ones(var.shape) * radius )
-#                         else:
-#                             if radius.shape != var.shape:
-#                                 raise ValueError("The given radius has shape '{}'. It should "
-#                                                  "either be scalar, or of shape '{}'."
-#                                                  .format(radius.shape, var.shape))
-#                             radii.append(radius)
-#                 rescaled_vals = [val * radius for val, radius in zip(unit_vals, radii)]
-#
-#                 # add the centre
-#                 centre = []
-#                 for varname, var in curvals.items():
-#                     comp = init_vals.centre[varname]
-#                     if shim.isscalar(var):
-#                         centre.append(comp)
-#                     else:
-#                         if shim.isscalar(comp):
-#                             centre.append( np.ones(var.shape) * comp )
-#                         else:
-#                             if comp.shape != var.shape:
-#                                 raise ValueError("The given centre component has shape '{}'. It should "
-#                                                  "either be scalar, or of shape '{}'."
-#                                                  .format(comp.shape, var.shape))
-#                             centre.append(comp)
-#                 _init_vals_values = [c + r for c, r in zip(centre, rescaled_vals)]
-#
-#                 # construct the initial value dictionary
-#                 _init_vals_dict = { sgd.get_param(name): val
-#                                     for name, val in zip(curvals.keys(),
-#                                                          _init_vals_values) }
-#
-#             sgd.initialize(_init_vals_dict, fitmask)
-#                 # Specifying fitmask ensures that parameters which
-#                 # are not being fit are left at the ground truth value
-#
-#     else:
-#         # Create the SGD from the loaded raw data
-#         sgd = gd.SGD_old(
-#             cost = model.loglikelihood,
-#             cost_format = 'logL',
-#             optimizer = params.optimizer,
-#             model = model,
-#             sgd_file = prev_run,
-#             set_params = False  # Have to call verify_transforms first
-#                 # FIXME: Use default (True) once verify_transforms no longer needed
-#         )
-#         sgd.verify_transforms(trust_automatically=True)
-#         sgd.set_params_to_evols()
-#
-#     # SGD instance has been either created or reloaded
-#     # It's not compiled yet: caller may find that the fit is already done, and not need to be compiled
-#
-#     return sgd
-#
-# def get_fitmask(model, mask_desc):
-#     return { getattr(model.params, name) : mask for name, mask in mask_desc.items() }
+def old_get_param_hierarchy(params):
+    sgd_params = copy.deepcopy(params)
+    # Parameters we never care about saving
+    del sgd_params['max_iterations']
+    # Initial value parameters - deepest level
+    init_params = sgd_params.init_vals
+    del sgd_params['init_vals']
+    # Dataset parameters
+    data_keys = ['data', 'input', 'model']
+    post_params = getattr(sgd_params, 'posterior', sgd_params)
+        # Really old scripts didn't have a 'posterior' key
+    data_params = ParameterSet({key: post_params[key] for key in data_keys})
+    for key in data_keys:
+        del post_params[key]
+
+    return [data_params, sgd_params, init_params]
 
 def get_param_hierarchy(params):
     """
     Return a list of mutually exclusive ParameterSets, each subsequent set corresponding to
     a nested directory.
     """
-    # SGD parameters
-    sgd_params = copy.deepcopy(params.sgd)
-    sgd_params.pop('max_iterations', None)  # Don't use max_iterations for filename
-    # Initial parameter values
-    init_params = copy.deepcopy(params.init_vals)
-    # Dataset parameters
-    data_keys = ['data', 'input', 'model']
-    data_params = copy.deepcopy(
-        ParameterSet({key: params.posterior[key] for key in data_keys}))
+    if 'sgd' not in params:
+        return old_get_param_hierarchy(params)
+    else:
+        # SGD parameters
+        sgd_params = copy.deepcopy(params.sgd)
+        sgd_params.pop('max_iterations', None)  # Don't use max_iterations for filename
+        # Initial parameter values
+        init_params = copy.deepcopy(params.init_vals)
+        # Dataset parameters
+        data_keys = ['data', 'input', 'model', 'mask']
+        data_params = copy.deepcopy(
+            ParameterSet({key: params.posterior[key] for key in data_keys}))
 
-    return [data_params, sgd_params, init_params]
+        return [data_params, sgd_params, init_params]
 
 def get_previous_run(params, resume=True):
     # Now load the sgd file
